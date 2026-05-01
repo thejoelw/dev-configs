593a7a9c8ae4fad6aaa6d0e5d10c988a9efe9398
Joel Walker <thejoeldw@gmail.com>
2026-04-30 20:06:02 -0700

    Add claude-hud config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ROOT=$(dirname "$0")
 
 sh $ROOT/_copy.sh $ROOT/configs/zed/settings.json ~/.config/zed/settings.json
 sh $ROOT/_copy.sh $ROOT/configs/claude/settings.json ~/.claude/settings.json
+sh $ROOT/_copy.sh $ROOT/configs/claude/plugins/claude-hud/config.json ~/.claude/plugins/claude-hud/config.json
 sh $ROOT/_copy.sh $ROOT/configs/tmux/tmux.conf ~/.tmux.conf
 sh $ROOT/_copy.sh $ROOT/configs/fish/config.fish ~/.config/fish/conf.d/joel.fish
 sh $ROOT/_copy.sh $ROOT/configs/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
--- save.sh
+++ save.sh
@@ -4,6 +4,7 @@ ROOT=$(dirname "$0")
 
 sh $ROOT/_copy.sh ~/.config/zed/settings.json $ROOT/configs/zed/settings.json
 sh $ROOT/_copy.sh ~/.claude/settings.json $ROOT/configs/claude/settings.json
+sh $ROOT/_copy.sh ~/.claude/plugins/claude-hud/config.json $ROOT/configs/claude/plugins/claude-hud/config.json
 sh $ROOT/_copy.sh ~/.tmux.conf $ROOT/configs/tmux/tmux.conf
 sh $ROOT/_copy.sh ~/.config/fish/conf.d/joel.fish $ROOT/configs/fish/config.fish
 sh $ROOT/_copy.sh ~/.config/fish/functions/fish_prompt.fish $ROOT/configs/fish/fish_prompt.fish
