7f81a390308336a60083c2ab6e7e108f342e5229
Joel Walker <thejoeldw@gmail.com>
2026-04-30 19:51:57 -0700

    Added ghostty config

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ sh $ROOT/_copy.sh $ROOT/configs/zed/settings.json ~/.config/zed/settings.json
 sh $ROOT/_copy.sh $ROOT/configs/claude/settings.json ~/.claude/settings.json
 sh $ROOT/_copy.sh $ROOT/configs/tmux/tmux.conf ~/.tmux.conf
 sh $ROOT/_copy.sh $ROOT/configs/fish/config.fish ~/.config/fish/conf.d/joel.fish
+sh $ROOT/_copy.sh $ROOT/configs/ghostty/config "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
--- save.sh
+++ save.sh
@@ -6,3 +6,4 @@ sh $ROOT/_copy.sh ~/.config/zed/settings.json $ROOT/configs/zed/settings.json
 sh $ROOT/_copy.sh ~/.claude/settings.json $ROOT/configs/claude/settings.json
 sh $ROOT/_copy.sh ~/.tmux.conf $ROOT/configs/tmux/tmux.conf
 sh $ROOT/_copy.sh ~/.config/fish/conf.d/joel.fish $ROOT/configs/fish/config.fish
+sh $ROOT/_copy.sh "$HOME/Library/Application Support/com.mitchellh.ghostty/config" $ROOT/configs/ghostty/config
