4d086af8b48a13b2295b4e8b9f4c53b31bf1eacb
Joel Walker <thejoeldw@gmail.com>
2026-04-30 19:56:01 -0700

    Add fish prompt

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ sh $ROOT/_copy.sh $ROOT/configs/zed/settings.json ~/.config/zed/settings.json
 sh $ROOT/_copy.sh $ROOT/configs/claude/settings.json ~/.claude/settings.json
 sh $ROOT/_copy.sh $ROOT/configs/tmux/tmux.conf ~/.tmux.conf
 sh $ROOT/_copy.sh $ROOT/configs/fish/config.fish ~/.config/fish/conf.d/joel.fish
+sh $ROOT/_copy.sh $ROOT/configs/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 sh $ROOT/_copy.sh $ROOT/configs/ghostty/config "$HOME/Library/Application Support/com.mitchellh.ghostty/config"
